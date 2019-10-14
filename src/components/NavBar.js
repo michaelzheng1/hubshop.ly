2a29efbd782d97ec8b015150174648c397bc1cd6
michaelzheng1 <mzheng1@terpmail.umd.edu>
2019-10-14 11:02:50 -0400

    add structure to navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+class NavBar extends React.Component {
+    render() {
+        return (
+            <div className="navBar-container">
+                NavBar
+            </div>
+        );
+    }
+}
+
+export default NavBar;
