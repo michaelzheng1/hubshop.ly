523322d3445fa5f890ff73b0565e41b1f60374aa
michaelzheng1 <mzheng1@terpmail.umd.edu>
2019-10-14 12:51:45 -0400

    add the details in status section

--- src/components/Status/Status.js
+++ src/components/Status/Status.js
@@ -5,9 +5,28 @@ class Status extends React.Component {
     render() {
         return(
             <div className="status">
-                <h1>Set-ups</h1>
+                <h1 className="setup-title">Set-ups</h1>
                 <div className="status-container">
                     <h1>Status</h1>
+                    <div className="hubspot-id">
+                        <p className="section-title">Hubspot ID</p>
+                        <h4 className="section-value">123456</h4>
+                        <div className="connection">Conntected</div>
+                    </div>
+                    <div className="js-code">
+                        <p className="section-title">JS Code Connection</p>
+                        <h4 className="section-value">NULL</h4>
+                        <div className="connection"> Not connected</div>
+                    </div>
+                    <div className="total-orders">
+                        <p className="section-title">Total Orders</p>
+                        <h4 className="section-value">15</h4>
+                    </div>
+                    <div className="total-contacts">
+                        <p className="section-title">Total Contacts</p>
+                        <h4 className="section-value">30</h4>
+                        <button>Add contacts</button>
+                    </div>
                 </div>
             </div>
         );
