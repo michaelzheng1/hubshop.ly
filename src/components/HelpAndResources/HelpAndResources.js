7d771b049368d5825aa9a2718f576c55615917f4
michaelzheng1 <mzheng1@terpmail.umd.edu>
2019-10-16 10:06:50 -0400

    remove the headers

--- src/components/HelpAndResources/HelpAndResources.js
+++ src/components/HelpAndResources/HelpAndResources.js
@@ -4,7 +4,6 @@ class HelpAndResources extends React.Component {
     render() {
         return (
             <div className="helpResource">
-                <h1>Help And Resources Section</h1>
             </div>
         );
     }
--- src/components/Status/Status.js
+++ src/components/Status/Status.js
@@ -4,9 +4,7 @@ import './Status.css';
 class Status extends React.Component {
     render() {
         return(
-            <div className="status">
-                <h1>Set-ups</h1>
-                
+            <div className="status">                
             </div>
         );
     }
