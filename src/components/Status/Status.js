8e6fe2216dfa9575403a363c72272ddaa6f06e5a
dmonyei <dmonyei@terpmail.umd.edu>
2019-10-24 15:00:03 -0400

    status features

--- src/components/Status/Status.js
+++ src/components/Status/Status.js
@@ -4,11 +4,24 @@ import './Status.css';
 class Status extends React.Component {
     render() {
         return(
-            <div className="status">
-                <div className="status-container">
-                <h1>Status</h1>           
-                </div>     
-            </div>
+          <div className="status">
+              <div className="status-container">
+              <h1>Status</h1>
+              <div className="status-info">
+                <div>HubSpot ID</div>
+                <div>
+                HubSpot Connection
+                <br/>
+                <button>Reconnect</button>
+                </div>
+                <div>
+                HubShop.ly Connection
+                <br/>
+                <button>Reconnect now</button>
+                </div>
+              </div>
+              </div>
+          </div>
         );
     }
 }
