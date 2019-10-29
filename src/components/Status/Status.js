1e17fa6614835cd24df6e96687037a5b3f93351c
dmonyei <dmonyei@terpmail.umd.edu>
2019-10-29 15:09:02 -0400

    code section added to status

--- src/components/Status/Status.js
+++ src/components/Status/Status.js
@@ -33,6 +33,30 @@ class Status extends React.Component {
             </div>
           </div>
         </div>
+        <div className="flex-container code">
+          <div className="code">
+            <h1>JS Code Install</h1>
+            <ol>
+              <li>Copy the Code and paste it right before the in your
+              themes HTML Head.html template
+              <input></input>
+              </li>
+              <li> Next, install the tag required for abandoned card
+              functionality. Copy the script and paste it at the very end of
+              your theme’s cartitem.html and check out cartitem.html snippets
+              <input></input>
+              </li>
+              <li> Please make sure to load your front page after installing.
+              <button>Recheck JavaScript Connection Status</button>
+              </li>
+            </ol>
+          </div>
+        </div>
+        <div className="flex-container config">
+          <div className="configure">
+            <h1>Configuration & Billing</h1>
+          </div>
+        </div>
       </div>
     );
   }
