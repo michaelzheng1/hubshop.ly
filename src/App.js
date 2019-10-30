4a0bae1e5f6725c2fbe5d7ca334b70ba20f8382b
michaelzheng1 <mzheng1@terpmail.umd.edu>
2019-10-30 09:55:36 -0400

    move the install section to the setup.js and fix the add the <head> in paragraph

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import NavBar from './components/NavBar/NavBar';
 import Header from './components/Header/Header';
 import Status from './components/Status/Status';
 import Setup from './components/Setup/Setup';
+import Billing from './components/Billing/Billing';
 import HelpAndResources from './components/HelpAndResources/HelpAndResources';
 import Blog from './components/Blog/Blog';
 import './App.css';
@@ -14,10 +15,9 @@ class App extends React.Component {
             <BrowserRouter>
                 <div className="main-container">
                     <Header />
-                    <Route exact path="/" component={Status} />
-                    <Route exact path="/set-up" component={Setup} />
-                    <Route exact path="/help-and-resources" component={HelpAndResources} />
-                    <Route exact path="/blog" component={Blog} />
+                    <Status />
+                    <Setup />
+                    <Billing />
                 </div>
             </BrowserRouter>
         );
--- src/components/Setup/Setup.js
+++ src/components/Setup/Setup.js
@@ -1,14 +1,39 @@
-import React from 'react';
-import './Setup.css';
+import React from "react";
+import "./Setup.css";
 
 class Setup extends React.Component {
-    render() {
-        return (
-            <div className="setup">
-                <h1>setup</h1>
-            </div>
-        );
-    }
+  render() {
+    var headHTML = "</head>";
+
+    return (
+      <div className="flex-container code">
+        <div className="code">
+          <h1>JS Code Install</h1>
+          <ol>
+            <li>
+              Copy the Code and paste it right before the {headHTML} in
+              <a href="#">your themes HTML Head.html template</a>
+              <input></input>
+            </li>
+            <li>
+              {" "}
+              Next, install the tag required for abandoned card functionality.
+              Copy the script and paste it at the very end of
+              <a href="#"> your theme’s cartitem.html</a> and 
+              <a href="#"> check out cartitem.html snippets</a>
+              
+              <input></input>
+            </li>
+            <li>
+              {" "}
+              Please make sure to load your front page after installing.
+              <button>Recheck JavaScript Connection Status</button>
+            </li>
+          </ol>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Setup;
