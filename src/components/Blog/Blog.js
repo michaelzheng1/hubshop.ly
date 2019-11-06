edba8921f5733f04ae8c1c4b27876aeb301f0227
michaelzheng1 <mzheng1@terpmail.umd.edu>
2019-11-05 23:09:49 -0500

    use card for blog.js

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,25 +1,34 @@
-import React from 'react';
+import React from "react";
 
 class Blog extends React.Component {
-    render() {
-        return (
-
-            <div className="blog">
-              <h1>Blog</h1>
-              <p>
-                <h2>Big Commerce Pricing: Choose the right plan for you</h2>
-                The more you know about your ecommerce customers, the better
-                you'll be able to create value for them in your products and
-                your marketing. The more value you create for your customers,
-                the more value they'll create...
-                <button type="button" class="btn btn-primary">Read Article
-                </button>
+  render() {
+    return (
+      <div class="container">
+        <div class="col-16">
+          <h1>Blog</h1>
+          <div className="card">
+            <h5 class="card-header">Blog</h5>
+            <div class="card-body">
+              <p class="card-text">
+                <p>
+                  <h2>Big Commerce Pricing: Choose the right plan for you</h2>
+                  The more you know about your ecommerce customers, the better
+                  you'll be able to create value for them in your products and
+                  your marketing. The more value you create for your customers,
+                  the more value they'll create...
+                  <button type="button" class="btn btn-primary">
+                    Read Article
+                  </button>
+                </p>
+                <img src="/src/components/Images/bigcommerce.png" />
+                <span>View more on GrooveCommerce Blog</span>
               </p>
-              <img src="/src/components/Images/bigcommerce.png"/>
-              <span>View more on GrooveCommerce Blog</span>
             </div>
-        );
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Blog;
