e25726866ef720d7db2757f1feb1f062ed09407a
michaelzheng1 <mzheng1@terpmail.umd.edu>
2019-11-06 12:28:56 -0500

    made the deals dropdown menu full width of the div

--- src/components/Billing/Billing&Deals.js
+++ src/components/Billing/Billing&Deals.js
@@ -29,7 +29,7 @@ class Billing extends React.Component {
                     <div className="col-md-6">
                       <div class="dropdown">
                         <button
-                          class="btn btn-secondary"
+                          class="btn btn-secondary dropdownMenuButton"
                           type="button"
                           id="dropdownMenuButton"
                           data-toggle="dropdown"
@@ -59,39 +59,32 @@ class Billing extends React.Component {
 
                   <hr className="horizontal-line"></hr>
 
-
                   <div className="bill">
-                  <h4>Payment Method</h4>
-                  <div className="bill-section">
-
-                    <div className="row">
-                      <div className="col-md-6">
-                        <p>MasterCard ending in 0000</p>
+                    <h4>Payment Method</h4>
+                    <div className="bill-section">
+                      <div className="row">
+                        <div className="col-md-6">
+                          <p>MasterCard ending in 0000</p>
+                        </div>
+                        <div className="col-md-6">
+                          <p className="right-side">Edit</p>
+                        </div>
                       </div>
-                      <div className="col-md-6">
-                        <p className="right-side">Edit</p>
+                      <div className="row">
+                        <div className="col-md-6">
+                          <p>Expires on 7/20</p>
+                        </div>
+                        <div className="col-md-6">
+                          <p className="right-side">Delete</p>
+                        </div>
                       </div>
-                    </div>
-                    <div className="row">
-                      <div className="col-md-6">
-                        <p>Expires on 7/20</p>
-                      </div>
-                      <div className="col-md-6">
-                        <p className="right-side">Delete</p>
-                      </div>
-                    </div>
                       <p className="right-side">Add Payment Method</p>
-                  </div>
+                    </div>
                   </div>
                 </p>
               </div>
             </div>
           </div>
-
-
-
-
-
 
           <div className="col-sm-6 ">
             <div className="card bill-and-deals">
@@ -99,88 +92,154 @@ class Billing extends React.Component {
               <div className="card-body">
                 <p className="card-text">
                   <div className="payment">
-                    <div className="sync">
-                      <p>
-                        Would you like HubShop.ly to sync your orders as Deals
-                        in Hubspot?
-                      </p>
-
-                      <div class="custom-control custom-switch">
-                        <input
-                          type="checkbox"
-                          class="custom-control-input"
-                          id="customSwitches"
-                        />
-                        <label
-                          class="custom-control-label"
-                          for="customSwitches"
-                        ></label>
+                    <div class="row">
+                      <div class="col-md-10">
+                        <p>
+                          Would you like HubShop.ly to sync your orders as Deals
+                          in Hubspot?
+                        </p>
+                      </div>
+                      <div class="col-md-2">
+                        <div class="custom-control custom-switch">
+                          <input
+                            type="checkbox"
+                            class="custom-control-input"
+                            id="customSwitches"
+                          />
+                          <label
+                            class="custom-control-label"
+                            for="customSwitches"
+                          ></label>
+                        </div>
                       </div>
                     </div>
-                    <div className="question">
-                      <p>
-                        Would you like to associate all orders to a specific
-                        pipeline?
-                      </p>
-                      <select className="mdb-select md-form">
-                        <option value="" disabled selected>
-                          Choose your option
-                        </option>
-                        <option value="1">Option 1</option>
-                        <option value="2">Option 2</option>
-                        <option value="3">Option 3</option>
-                      </select>
+
+                    <p>
+                      Would you like to associate all orders to a specific
+                      pipeline?
+                    </p>
+
+                    <div class="dropdown">
+                      <button
+                        className="btn btn-secondary btn-block dropdownMenuButtonDeals"
+                        type="button"
+                        data-toggle="dropdown"
+                        aria-haspopup="true"
+                        aria-expanded="false"
+                      >
+                        Sales Pipline
+                        <img src="/src/components/Images/arrow_down.png" />
+                      </button>
+                      <div
+                        class="dropdown-menu"
+                        aria-labelledby="dropdownMenuButton"
+                      >
+                        <a class="dropdown-item" href="#">
+                          Action
+                        </a>
+                      </div>
                     </div>
+
                     <div className="question">
                       <p>When an order is completed Put it in</p>
-                      <select className="mdb-select md-form">
-                        <option value="" disabled selected>
-                          Choose your option
-                        </option>
-                        <option value="1">Option 1</option>
-                        <option value="2">Option 2</option>
-                        <option value="3">Option 3</option>
-                      </select>
                     </div>
+                    <div class="dropdown">
+                      <button
+                        className="btn btn-secondary btn-block dropdownMenuButtonDeals"
+                        type="button"
+                        data-toggle="dropdown"
+                        aria-haspopup="true"
+                        aria-expanded="false"
+                      >
+                        Closed Won
+                        <img src="/src/components/Images/arrow_down.png" />
+                      </button>
+                      <div
+                        class="dropdown-menu"
+                        aria-labelledby="dropdownMenuButton"
+                      >
+                        <a class="dropdown-item" href="#">
+                          Action
+                        </a>
+                      </div>
+                    </div>
+
                     <div className="question">
                       <p>When an order is cancelled, Put it in </p>
-                      <select className="mdb-select md-form">
-                        <option value="" disabled selected>
-                          Choose your option
-                        </option>
-                        <option value="1">Option 1</option>
-                        <option value="2">Option 2</option>
-                        <option value="3">Option 3</option>
-                      </select>
                     </div>
+                    <div class="dropdown">
+                      <button
+                        className="btn btn-secondary btn-block dropdownMenuButtonDeals"
+                        type="button"
+                        data-toggle="dropdown"
+                        aria-haspopup="true"
+                        aria-expanded="false"
+                      >
+                        Closed Lost
+                        <img src="/src/components/Images/arrow_down.png" />
+                      </button>
+                      <div
+                        class="dropdown-menu"
+                        aria-labelledby="dropdownMenuButton"
+                      >
+                        <a class="dropdown-item" href="#">
+                          Action
+                        </a>
+                      </div>
+                    </div>
+
                     <div className="question">
                       <p>
                         Would you like to associate all orders to a Specific
                         Deal Type?
                       </p>
-                      <select className="mdb-select md-form">
-                        <option value="" disabled selected>
-                          Choose your option
-                        </option>
-                        <option value="1">Option 1</option>
-                        <option value="2">Option 2</option>
-                        <option value="3">Option 3</option>
-                      </select>
                     </div>
+                    <div class="dropdown">
+                      <button
+                        className="btn btn-secondary btn-block dropdownMenuButtonDeals"
+                        type="button"
+                        data-toggle="dropdown"
+                        aria-haspopup="true"
+                        aria-expanded="false"
+                      >
+                        Closed Lost
+                        <img src="/src/components/Images/arrow_down.png" />
+                      </button>
+                      <div
+                        class="dropdown-menu"
+                        aria-labelledby="dropdownMenuButton"
+                      >
+                        <a class="dropdown-item" href="#">
+                          Action
+                        </a>
+                      </div>
+                    </div>
+
                     <div className="question">
                       <p>
                         Would you like to associate all order to a Specific
                         HubSpot User?
                       </p>
-
-                      <select className="mdb-select md-form">
-                        <option value="" disabled selected>
-                          Choose your option
-                        </option>
-                        <option value="1">Option 1</option>
-                        <option value="2">Option 2</option>
-                        <option value="3">Option 3</option>
-                      </select>
+                    </div>
+                    <div class="dropdown">
+                      <button
+                        className="btn btn-secondary btn-block dropdownMenuButtonDeals"
+                        type="button"
+                        data-toggle="dropdown"
+                        aria-haspopup="true"
+                        aria-expanded="false"
+                      >
+                        Closed Lost
+                        <img src="/src/components/Images/arrow_down.png" />
+                      </button>
+                      <div
+                        class="dropdown-menu"
+                        aria-labelledby="dropdownMenuButton"
+                      >
+                        <a class="dropdown-item" href="#">
+                          Action
+                        </a>
+                      </div>
                     </div>
                   </div>
                 </p>
